af21452af7dcaf1d3aa2b8dc4d8c2ac9dcf04058
Sukma Gladys V <sukmagladysv@gmail.com>
2023-02-20 10:05:05 +0700

    Praktikum 2 - no 3

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function index() {
+        return 'Nama : Sukma Gladys Vellanie <br> NIM : 2141720179';
+       } 
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,9 +18,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', [PageController::class, 'index']);
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/about', [PageController::class, 'about']);
+Route::get('/about', [AboutController::class, 'index']);
 
-Route::get('/articles/{id}', [PageController::class, 'show']);
+Route::get('/articles/{id}', [ArticleController::class, 'index']);
 
