126a33270cf663e4e5c60709ce4aca17be6d70d9
Sukma Gladys V <sukmagladysv@gmail.com>
2023-02-20 09:41:24 +0700

    Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
-Route::get('/', function (){
-    echo "Selamat Datang";
-});
+Route::get('/', [PageController::class, 'index']);
 
-Route::get('/about', function (){
-    echo "NIM : 2141720179 <br> Nama : Sukma Gladys Vellanie";
-});
+Route::get('/about', [PageController::class, 'about']);
 
-Route::get('/articles/{id}', function ($id){
-    echo "Halaman Artikel dengan ID ".$id;
-});
+Route::get('/articles/{id}', [PageController::class, 'show']);
 
